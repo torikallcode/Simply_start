b67446c51e87aeee4d9142ff0b2977d7d242a516
torikallcode <torikalcontent@gmail.com>
2025-02-17 21:46:17 +0800

    create handler GetTask

--- backend/handlers/handler.go
+++ backend/handlers/handler.go
@@ -3,8 +3,12 @@ package handlers
 import (
 	"backend/database"
 	"backend/models"
+	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func GetAllTime(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +46,8 @@ func GetAllTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	defer rows.Close()
+
 	for rows.Next() {
 		var task models.Task
 		if err := rows.Scan(&task.ID, &task.Name_task, &task.From_time, &task.To_time, &task.Content, &task.Status); err != nil {
@@ -51,4 +57,26 @@ func GetAllTask(w http.ResponseWriter, r *http.Request) {
 		tasks = append(tasks, task)
 	}
 	json.NewEncoder(w).Encode(tasks)
+}
+
+func GetTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "applications/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task", http.StatusBadRequest)
+		return
+	}
+
+	var task models.Task
+	query := "SELECT id, name_task, from_time, to_time, content, status FROM task WHERE id = ?"
+	err = database.DB.QueryRow(query, id).Scan(&task.ID, &task.Name_task, &task.From_time, &task.To_time, &task.Content, &task.Status)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Rows not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(task)
 }
