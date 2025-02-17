b4debb9b2914a7b28ef66c73ac8505b939ce43ba
torikallcode <torikalcontent@gmail.com>
2025-02-17 09:48:07 +0800

    create handler GetAllTask

--- backend/handlers/handler.go
+++ backend/handlers/handler.go
@@ -30,3 +30,25 @@ func GetAllTime(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(times)
 }
+
+func GetAllTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var tasks []models.Task
+
+	query := "SELECT id, name_task, from_time, to_time, content, status FROM task"
+	rows, err := database.DB.Query(query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for rows.Next() {
+		var task models.Task
+		if err := rows.Scan(&task.ID, &task.Name_task, &task.From_time, &task.To_time, &task.Content, &task.Status); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tasks = append(tasks, task)
+	}
+	json.NewEncoder(w).Encode(tasks)
+}
