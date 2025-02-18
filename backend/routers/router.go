2a176a1e9d4a1e9c82d16dfddfd1e92e6adf5b34
torikallcode <torikalcontent@gmail.com>
2025-02-18 22:22:13 +0800

    update routers

--- backend/routers/router.go
+++ backend/routers/router.go
@@ -12,10 +12,10 @@ func SetupRouter() *mux.Router {
 	router.HandleFunc("/time", handlers.GetAllTime).Methods("GET")
 	router.HandleFunc("/task", handlers.GetAllTask).Methods("GET")
 	router.HandleFunc("/task/{id}", handlers.GetTask).Methods("GET")
-	router.HandleFunc("/task", handlers.CreateTask).Methods("POST")
+	router.HandleFunc("/task", handlers.CreateTask).Methods("POST", "OPTIONS")
 	router.HandleFunc("/task/{id}", handlers.UpdateTask).Methods("PUT")
 	router.HandleFunc("/task/{id}", handlers.DeleteTask).Methods("DELETE")
-	router.HandleFunc("/task/{id}", handlers.UpdateTaskStatus).Methods("PUT")
+	router.HandleFunc("/task/{id}/status", handlers.UpdateTaskStatus).Methods("PATCH")
 
 	return router
 
