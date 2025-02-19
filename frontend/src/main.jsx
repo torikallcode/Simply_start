b123dcdd5475c4f4422e53d28f70f1f203c1978d
torikallcode <torikalcontent@gmail.com>
2025-02-19 21:18:25 +0800

    update router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Home } from './pages/home'
+import { DetailTask } from './pages/DetailTask'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  }, {
+    path: 'detail/:id',
+    element: <DetailTask />
   }
 ])
 
